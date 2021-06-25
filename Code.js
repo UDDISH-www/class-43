691fcba335b27bbd9021ac00957a9974ead6fd96
UDDISH-www <73113673+UDDISH-www@users.noreply.github.com>
2021-06-25 12:14:14 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+fill("white")
+textSize(15)
+text("U C T F N N O I", 700,150)
+fill("lightblue")
+text("Hint: Performs a perticular task !!", 700,170)
+
+fill("white")
+    textSize(15)
+    text("B T A D E S A A", 100,400)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,420)
+
+}
